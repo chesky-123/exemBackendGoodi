cd4e39f374eebfb57e263648655c232c9503f21e
chesky <sye0534184736@gmail.com>
2026-07-29 09:58:54 +0300

    create basic route

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+import express from 'express';
+import 'dotenv/config';
+import { router } from './routes/router.js';
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/",router);
+
+app.listen(PORT,(e)=>{
+    if (e) return console.error(e.message);
+    console.log(`server running in port ${PORT}`);
+    
+})
 
 
 
