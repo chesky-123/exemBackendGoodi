3702677358c4d79fa132fea3013eaa2624122d8a
chesky <sye0534184736@gmail.com>
2026-07-29 10:28:13 +0300

    create endpoint sistem

--- routes/router.js
+++ routes/router.js
@@ -3,4 +3,42 @@ import express from 'express';
 export const router = express.Router()
 
 
+router.post("/soldiers/:soldierId/benefits", (req, res) => {
+    try {
+        
+        res.json({})
+    } catch (error) {
+        console.error(e.message);
+        
+        res.json({})
+    }
+});
+
+router.get("/soldiers/:soldierId/benefits", (req, res) => {
+    res.json({})
+});
+
+router.patch("/soldiers/:soldierId/benefits", (req, res) => {
+    res.json({})
+});
+
+router.post("/budget", (req, res) => {
+    res.json({})
+});
+
+router.get("/budget", (req, res) => {
+    res.json({})
+});
+
+router.get("/budget:id/transactions", (req, res) => {
+    res.json({})
+});
+
+router.post("/budget:id/spend", (req, res) => {
+    res.json({})
+});
+
+
+
+
 
