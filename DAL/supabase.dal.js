4c6a415fc837e932558629550f2ed441c3597f76
chesky <sye0534184736@gmail.com>
2026-07-29 12:02:06 +0300

    connect to supabase

--- DAL/supabase.dal.js
+++ DAL/supabase.dal.js
@@ -1,5 +1,10 @@
+import { createClient } from "@supabase/supabase-js"
 
+const SUPBASE_URL = process.env.SUPBASE_URL;
 
+const API_KEY = process.env.API_KEY;
+
+export const client = createClient(SUPBASE_URL,API_KEY);
 
 
 
