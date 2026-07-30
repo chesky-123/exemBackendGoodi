7e91f2f3c3d0938597c887bd6f8d5ffcc9eb7405
chesky <sye0534184736@gmail.com>
2026-07-30 11:40:02 +0300

    fix the GET budget endpoint

--- DAL/supabase.dal.js
+++ DAL/supabase.dal.js
@@ -1,3 +1,4 @@
+// import { array } from "node:stream/iter";
 import { sbClient } from "../db/supabaseDb.js";
 
 
@@ -7,47 +8,72 @@ export async function insertBuget(newData) {
     const { data, error } = await sbClient.from('budgets').insert(newData).select()
     if (error) return console.error(error.message);
 
-    
+
     return data;
 };
 
 
 export async function getBugetByIdFromDb(budgetId) {
-    
-    const { data,error} = await sbClient.from('spend_transaction').select().eq('budgetId',budgetId);
+
+    const { data, error } = await sbClient.from('budgets').select().eq('id', budgetId);
     // console.log('data'
     // ,data);
-    
-    if (error) { return console.error(error.message);}
+
+    if (error) { return console.error(error.message); }
     return data;
 };
 
-export async function updateAllocatedAmount(id,amount) {
+export async function updateAllocatedAmount(id, amount) {
     const newdata = await getBugetByIdFromDb(id);
-    console.log(typeof newdata[0].allocatedAmount , typeof amount);
-    
+    console.log(typeof newdata[0].allocatedAmount, typeof amount);
+
     newdata[0]['allocatedAmount'] = newdata[0].allocatedAmount - +amount;
     // console.log(newdata);
-    
-    const {data ,error} = await sbClient.from('budgets').update(newdata).eq("id",id).select();
-    if(error) return console.error(error.message);
-    
+
+    const { data, error } = await sbClient.from('budgets').update(newdata).eq("id", id).select();
+    if (error) return console.error(error.message);
+
     return data[0]
 }
 
 
 export async function insertBugetSpend(newData) {
-    const {data,error} = await sbClient.from("spend_transaction").insert(newData).select();
-    if(error) return console.error(error.message);
+    const { data, error } = await sbClient.from("spend_transaction").insert(newData).select();
+    if (error) return console.error(error.message);
     return data
-    
+
 };
 
 
 export async function getBudgetFromDb(conditions) {
+
+    const result = []
+    if (conditions.unit) {
+        const { data, error } = await sbClient.from('budgets').select().eq("unit", conditions.unit);
+        if (error) return console.error(error.message);
+        for (let i = 0; i < data.length; i++) {
+            if (!result.includes(data[i])) { result.push(data[i]) };
     
-    const {data,error} = await sbClient.from('budgets').select().eq("unit",conditions.unit);
+        }
+    }
     
-    if(error) return console.error(error.message);
-    return data;
+    if (conditions.month) {
+        const {data,error} = await sbClient.from('budgets').select().eq("month", conditions.month);
+        if (error) return console.error(error.message);
+        for (let i = 0; i < data.length; i++) {
+            if (!result.includes(data[i])) { result.push(data[i]) };
+    
+        }
+    }
+    
+    if (conditions.benefitType) {
+        const {data,error} = await sbClient.from('budgets').select().eq("benefitType", conditions.benefitType);
+        if (error) return console.error(error.message);
+        for (let i = 0; i < data.length; i++) {
+            if (!result.includes(data[i])) { result.push(data[i]) };
+    
+        }
+    }
+
+    return result;
 }
--- middlwares/supabase.js
+++ middlwares/supabase.js
@@ -14,4 +14,35 @@ export async function isId(req, res, next) {
         console.error(e.message);
         return res.status(500).json({ error: `server filed` })
     }
+    try {
+        const {id} = req.params;
+        if(!id) return res.status(400).json({error:'id not entered'});
+        const data = await getBugetByIdFromDb(id);
+        if(data.length === 0) return res.status(404).json({error:"not found"});
+        next()
+    } catch (e) {
+        console.error(e.message);
+        return res.status(500).json({ error: `server filed` })
+    }
 }
+
+export async function isValidQuery(req,res,next) {
+    try {
+        const VALID_FILTERS = ['unit','month','benefitType']
+        const filters = req.query;
+        const keysFilters = Object.keys(filters);
+        const isValidFilters = keysFilters.some(f => VALID_FILTERS.includes(f));
+        if (!isValidFilters) return res.status()
+        next()
+    } catch (e) {
+        console.error(e.message);
+        return res.status(500).json({ error: `server filed` })
+    }
+}
+
+
+
+
+
+
+
